681517bce3080fcea479d6b9067e2ce3483f1b83
Renato Lovato <renatolovato@gmail.com>
2024-07-29 14:17:17 -0400

    Initial commit

---
+++ src/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace GoogleContentApi;
+
+class Client
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function authenticate()
+    {
+
+    }
+
+    public function authorization()
+    {
+
+    }
+}
